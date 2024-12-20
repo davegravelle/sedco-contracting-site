a1ec0e183275ca9ce425c6c1eef2c19cfd2d502a
Dave Gravelle <dgravelle.dev@gmail.com>
2024-12-19 21:46:25 -0800

    updates for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap()],
 	image: {
 		service: { entrypoint: 'astro/assets/services/sharp' }
 	},
+	site: 'https://sedcocontracting.com ',
 });
